1a73537c8a90ecfa5cdb295b6bc61dc8ed7f4524
Varya Kush <barbarakushaeva@gmail.com>
2025-01-12 18:25:56 +0300

    Сделан адаптив для горизонтального расположения телефона

--- lib/adaptive.dart
+++ lib/adaptive.dart
@@ -18,86 +18,170 @@ class _AdaptiveState extends State<Adaptive> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              // ПРИМЕР РАБОТЫ GridView
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // Количество столбцов
-                  crossAxisSpacing: 10.0, // Отступ между столбцами
-                  mainAxisSpacing: 10.0, // Отступ между строками
-                  childAspectRatio: 1.0, // Соотношение сторон (ширина / высота)
-                ),
-                itemCount: 3, // Количество элементов
-                itemBuilder: (context, index) {
-                  // Список изображений
-                  final images = [
-                    'asset/jpeg/kitten.jpeg',
-                    'asset/jpeg/flower.jpeg',
-                    'asset/jpeg/discoball.jpeg',
-                  ];
-                  return GestureDetector(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return Dialog(
-                              child: Container(
-                            width: 200,
-                            height: 150,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Text(
-                                  'Вы нажали на фото!',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    color: CupertinoColors.black,
-                                    fontSize: 20,
+      body: SafeArea(child: LayoutBuilder(builder: (context, constraits) {
+        final flag = constraits.maxHeight > constraits.maxWidth;
+        if (flag) {
+          return Column(
+            children: [
+              Expanded(
+                // ПРИМЕР РАБОТЫ GridView
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // Количество столбцов
+                    crossAxisSpacing: 10.0, // Отступ между столбцами
+                    mainAxisSpacing: 10.0, // Отступ между строками
+                    childAspectRatio:
+                        1.0, // Соотношение сторон (ширина / высота)
+                  ),
+                  itemCount: 3, // Количество элементов
+                  itemBuilder: (context, index) {
+                    // Список изображений
+                    final images = [
+                      'asset/jpeg/kitten.jpeg',
+                      'asset/jpeg/flower.jpeg',
+                      'asset/jpeg/discoball.jpeg',
+                    ];
+                    return GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                                child: Container(
+                              width: 200,
+                              height: 150,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    height: 20,
                                   ),
-                                ),
-                                SizedBox(
-                                  height: 25,
-                                ),
-                                Align(
-                                    alignment: Alignment.bottomRight,
-                                    child: Padding(
-                                      padding: EdgeInsets.only(right: 20),
-                                      child: GestureDetector(
-                                        onTap: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text(
-                                          'Закрыть',
-                                          style: TextStyle(
-                                            color: CupertinoColors.black,
-                                            fontSize: 16,
+                                  Text(
+                                    'Вы нажали на фото!',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: CupertinoColors.black,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 25,
+                                  ),
+                                  Align(
+                                      alignment: Alignment.bottomRight,
+                                      child: Padding(
+                                        padding: EdgeInsets.only(right: 20),
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text(
+                                            'Закрыть',
+                                            style: TextStyle(
+                                              color: CupertinoColors.black,
+                                              fontSize: 16,
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ))
-                              ],
-                            ),
-                          ));
-                        },
-                      );
-                    },
-                    child: Image.asset(
-                      images[index],
-                      fit: BoxFit.cover,
-                    ),
-                  );
-                },
+                                      ))
+                                ],
+                              ),
+                            ));
+                          },
+                        );
+                      },
+                      child: Image.asset(
+                        images[index],
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          );
+        } else {
+          return Row(
+            children: [
+              Expanded(
+                // ПРИМЕР РАБОТЫ GridView
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3, // Количество столбцов
+                    crossAxisSpacing: 10.0, // Отступ между столбцами
+                    mainAxisSpacing: 10.0, // Отступ между строками
+                    childAspectRatio:
+                        1.0, // Соотношение сторон (ширина / высота)
+                  ),
+                  itemCount: 3, // Количество элементов
+                  itemBuilder: (context, index) {
+                    // Список изображений
+                    final images = [
+                      'asset/jpeg/kitten.jpeg',
+                      'asset/jpeg/flower.jpeg',
+                      'asset/jpeg/discoball.jpeg',
+                    ];
+                    return GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return Dialog(
+                                child: Container(
+                              width: 200,
+                              height: 150,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    'Вы нажали на фото!',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: CupertinoColors.black,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 25,
+                                  ),
+                                  Align(
+                                      alignment: Alignment.bottomRight,
+                                      child: Padding(
+                                        padding: EdgeInsets.only(right: 20),
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text(
+                                            'Закрыть',
+                                            style: TextStyle(
+                                              color: CupertinoColors.black,
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                        ),
+                                      ))
+                                ],
+                              ),
+                            ));
+                          },
+                        );
+                      },
+                      child: Image.asset(
+                        images[index],
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        }
+      })),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,55 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Adaptive(title: 'Adaptive'),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0; // Текущий индекс вкладки
+
+  final List<Widget> _screens = [
+    const Adaptive(title: 'Adaptive'), // Основной экран
+    Center(child: Text('Экран 2')),
+    Center(child: Text('Экран 3')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index; // Обновляем текущий индекс вкладки
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex], // Отображаем текущий экран
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex, // Устанавливаем текущий индекс вкладки
+        onTap: _onItemTapped, // Обработчик нажатия на вкладку
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Главная',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Поиск',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Избранное',
+          ),
+        ],
+      ),
     );
   }
 }
