1400006c2ceca5a2744f0d07eede71347bea7958
Varya Kush <barbarakushaeva@gmail.com>
2025-01-12 20:07:37 +0300

    Настроен padding, добавлены картинки

--- lib/adaptive.dart
+++ lib/adaptive.dart
@@ -22,168 +22,186 @@ class _AdaptiveState extends State<Adaptive> {
       body: SafeArea(child: LayoutBuilder(builder: (context, constraits) {
         final flag = constraits.maxHeight > constraits.maxWidth;
         if (flag) {
-          return Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Expanded(
-                // ПРИМЕР РАБОТЫ GridView
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, // Количество столбцов
-                    crossAxisSpacing: 10.0, // Отступ между столбцами
-                    mainAxisSpacing: 10.0, // Отступ между строками
-                    childAspectRatio:
-                        1.0, // Соотношение сторон (ширина / высота)
+          return Padding(
+              padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
                   ),
-                  itemCount: 3, // Количество элементов
-                  itemBuilder: (context, index) {
-                    // Список изображений
-                    final images = [
-                      'asset/jpeg/kitten.jpeg',
-                      'asset/jpeg/flower.jpeg',
-                      'asset/jpeg/discoball.jpeg',
-                    ];
-                    return GestureDetector(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return Dialog(
-                                child: Container(
-                              width: 200,
-                              height: 150,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  Text(
-                                    'Вы нажали на фото!',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: CupertinoColors.black,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 25,
-                                  ),
-                                  Align(
-                                      alignment: Alignment.bottomRight,
-                                      child: Padding(
-                                        padding: EdgeInsets.only(right: 20),
-                                        child: GestureDetector(
-                                          onTap: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: Text(
-                                            'Закрыть',
-                                            style: TextStyle(
-                                              color: CupertinoColors.black,
-                                              fontSize: 16,
-                                            ),
-                                          ),
+                  Expanded(
+                    // ПРИМЕР РАБОТЫ GridView
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2, // Количество столбцов
+                        crossAxisSpacing: 20.0, // Отступ между столбцами
+                        mainAxisSpacing: 20.0, // Отступ между строками
+                        childAspectRatio:
+                            1.0, // Соотношение сторон (ширина / высота)
+                      ),
+                      itemCount: 9, // Количество элементов
+                      itemBuilder: (context, index) {
+                        // Список изображений
+                        final images = [
+                          'asset/jpeg/kitten.jpeg',
+                          'asset/jpeg/flower.jpeg',
+                          'asset/jpeg/discoball.jpeg',
+                          'asset/jpeg/kitten.jpeg',
+                          'asset/jpeg/flower.jpeg',
+                          'asset/jpeg/discoball.jpeg',
+                          'asset/jpeg/kitten.jpeg',
+                          'asset/jpeg/flower.jpeg',
+                          'asset/jpeg/discoball.jpeg',
+                        ];
+                        return GestureDetector(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return Dialog(
+                                    child: Container(
+                                  width: 200,
+                                  height: 150,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      Text(
+                                        'Вы нажали на фото!',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: CupertinoColors.black,
+                                          fontSize: 20,
                                         ),
-                                      ))
-                                ],
-                              ),
-                            ));
+                                      ),
+                                      SizedBox(
+                                        height: 25,
+                                      ),
+                                      Align(
+                                          alignment: Alignment.bottomRight,
+                                          child: Padding(
+                                            padding: EdgeInsets.only(right: 20),
+                                            child: GestureDetector(
+                                              onTap: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text(
+                                                'Закрыть',
+                                                style: TextStyle(
+                                                  color: CupertinoColors.black,
+                                                  fontSize: 16,
+                                                ),
+                                              ),
+                                            ),
+                                          ))
+                                    ],
+                                  ),
+                                ));
+                              },
+                            );
                           },
+                          child: Image.asset(
+                            images[index],
+                            fit: BoxFit.cover,
+                          ),
                         );
                       },
-                      child: Image.asset(
-                        images[index],
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          );
+                    ),
+                  ),
+                ],
+              ));
         } else {
-          return Row(
-            children: [
-              Expanded(
-                // ПРИМЕР РАБОТЫ GridView
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, // Количество столбцов
-                    crossAxisSpacing: 10.0, // Отступ между столбцами
-                    mainAxisSpacing: 10.0, // Отступ между строками
-                    childAspectRatio:
-                        1.0, // Соотношение сторон (ширина / высота)
-                  ),
-                  itemCount: 3, // Количество элементов
-                  itemBuilder: (context, index) {
-                    // Список изображений
-                    final images = [
-                      'asset/jpeg/kitten.jpeg',
-                      'asset/jpeg/flower.jpeg',
-                      'asset/jpeg/discoball.jpeg',
-                    ];
-                    return GestureDetector(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return Dialog(
-                                child: Container(
-                              width: 200,
-                              height: 150,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  Text(
-                                    'Вы нажали на фото!',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: CupertinoColors.black,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 25,
-                                  ),
-                                  Align(
-                                      alignment: Alignment.bottomRight,
-                                      child: Padding(
-                                        padding: EdgeInsets.only(right: 20),
-                                        child: GestureDetector(
-                                          onTap: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: Text(
-                                            'Закрыть',
-                                            style: TextStyle(
-                                              color: CupertinoColors.black,
-                                              fontSize: 16,
-                                            ),
-                                          ),
+          return Padding(
+              padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Column(
+                children: [
+                  Expanded(
+                    // ПРИМЕР РАБОТЫ GridView
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3, // Количество столбцов
+                        crossAxisSpacing: 20.0, // Отступ между столбцами
+                        mainAxisSpacing: 20.0, // Отступ между строками
+                        childAspectRatio:
+                            1.0, // Соотношение сторон (ширина / высота)
+                      ),
+                      itemCount: 9, // Количество элементов
+                      itemBuilder: (context, index) {
+                        // Список изображений
+                        final images = [
+                          'asset/jpeg/kitten.jpeg',
+                          'asset/jpeg/flower.jpeg',
+                          'asset/jpeg/discoball.jpeg',
+                          'asset/jpeg/kitten.jpeg',
+                          'asset/jpeg/flower.jpeg',
+                          'asset/jpeg/discoball.jpeg',
+                          'asset/jpeg/kitten.jpeg',
+                          'asset/jpeg/flower.jpeg',
+                          'asset/jpeg/discoball.jpeg',
+                        ];
+                        return GestureDetector(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return Dialog(
+                                    child: Container(
+                                  width: 200,
+                                  height: 150,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      Text(
+                                        'Вы нажали на фото!',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: CupertinoColors.black,
+                                          fontSize: 20,
                                         ),
-                                      ))
-                                ],
-                              ),
-                            ));
+                                      ),
+                                      SizedBox(
+                                        height: 25,
+                                      ),
+                                      Align(
+                                          alignment: Alignment.bottomRight,
+                                          child: Padding(
+                                            padding: EdgeInsets.only(right: 20),
+                                            child: GestureDetector(
+                                              onTap: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text(
+                                                'Закрыть',
+                                                style: TextStyle(
+                                                  color: CupertinoColors.black,
+                                                  fontSize: 16,
+                                                ),
+                                              ),
+                                            ),
+                                          ))
+                                    ],
+                                  ),
+                                ));
+                              },
+                            );
                           },
+                          child: Image.asset(
+                            images[index],
+                            fit: BoxFit.cover,
+                          ),
                         );
                       },
-                      child: Image.asset(
-                        images[index],
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          );
+                    ),
+                  ),
+                ],
+              ));
         }
       })),
     );
--- lib/chooseImg.dart
+++ lib/chooseImg.dart
@@ -14,7 +14,17 @@ class ChooseImg extends StatefulWidget {
 
 class _ChooseImgState extends State<ChooseImg> {
   // Список для отслеживания фокуса для каждой картинки
-  List<bool> _focusedList = [false, false, false];
+  List<bool> _focusedList = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
 
   void _setFocused(int index) {
     setState(() {
@@ -36,108 +46,121 @@ class _ChooseImgState extends State<ChooseImg> {
           builder: (context, constraints) {
             final flag = constraints.maxHeight > constraints.maxWidth;
             if (flag) {
-              return Column(
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Expanded(
-                    child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 10.0,
-                        mainAxisSpacing: 10.0,
-                        childAspectRatio: 1.0,
-                      ),
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        // Список изображений
-                        final images = [
-                          'asset/jpeg/kitten.jpeg',
-                          'asset/jpeg/flower.jpeg',
-                          'asset/jpeg/discoball.jpeg',
-                        ];
-                        return GestureDetector(
-                          onTap: () {
-                            _setFocused(
-                                index); // Переключаем состояние фокуса для выбранной картинки
-                          },
-                          child: FocusableActionDetector(
-                            onShowFocusHighlight: (focused) =>
-                                _setFocused(index),
-                            onShowHoverHighlight: (focused) =>
-                                _setFocused(index),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: _focusedList[index]
-                                    ? BorderRadius.circular(
-                                        32) // Округление для выбранной картинки
-                                    : BorderRadius.circular(
-                                        0), // Без округления для остальных
-                                image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(images[index]),
+              return Padding(
+                  padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 20.0,
+                            mainAxisSpacing: 20.0,
+                            childAspectRatio: 1.0,
+                          ),
+                          itemCount: 9,
+                          itemBuilder: (context, index) {
+                            // Список изображений
+                            final images = [
+                              'asset/jpeg/kitten.jpeg',
+                              'asset/jpeg/flower.jpeg',
+                              'asset/jpeg/discoball.jpeg',
+                              'asset/jpeg/kitten.jpeg',
+                              'asset/jpeg/flower.jpeg',
+                              'asset/jpeg/discoball.jpeg',
+                              'asset/jpeg/kitten.jpeg',
+                              'asset/jpeg/flower.jpeg',
+                              'asset/jpeg/discoball.jpeg',
+                            ];
+                            return GestureDetector(
+                              onTap: () {
+                                _setFocused(
+                                    index); // Переключаем состояние фокуса для выбранной картинки
+                              },
+                              child: FocusableActionDetector(
+                                onShowFocusHighlight: (focused) =>
+                                    _setFocused(index),
+                                onShowHoverHighlight: (focused) =>
+                                    _setFocused(index),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: _focusedList[index]
+                                        ? BorderRadius.circular(
+                                            32) // Округление для выбранной картинки
+                                        : BorderRadius.circular(
+                                            0), // Без округления для остальных
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(images[index]),
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              );
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ));
             } else {
-              return Row(
-                children: [
-                  Expanded(
-                    child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 10.0,
-                        mainAxisSpacing: 10.0,
-                        childAspectRatio: 1.0,
-                      ),
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        // Список изображений
-                        final images = [
-                          'asset/jpeg/kitten.jpeg',
-                          'asset/jpeg/flower.jpeg',
-                          'asset/jpeg/discoball.jpeg',
-                        ];
-                        return GestureDetector(
-                          onTap: () {
-                            _setFocused(
-                                index); // Переключаем состояние фокуса для выбранной картинки
-                          },
-                          child: FocusableActionDetector(
-                            onShowFocusHighlight: (focused) =>
-                                _setFocused(index),
-                            onShowHoverHighlight: (focused) =>
-                                _setFocused(index),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: _focusedList[index]
-                                    ? BorderRadius.circular(
-                                        32) // Округление для выбранной картинки
-                                    : BorderRadius.circular(
-                                        0), // Без округления для остальных
-                                image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(images[index]),
+              return Padding(
+                  padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 20.0,
+                            mainAxisSpacing: 20.0,
+                            childAspectRatio: 1.0,
+                          ),
+                          itemCount: 9,
+                          itemBuilder: (context, index) {
+                            // Список изображений
+                            final images = [
+                              'asset/jpeg/kitten.jpeg',
+                              'asset/jpeg/flower.jpeg',
+                              'asset/jpeg/discoball.jpeg',
+                              'asset/jpeg/kitten.jpeg',
+                              'asset/jpeg/flower.jpeg',
+                              'asset/jpeg/discoball.jpeg',
+                              'asset/jpeg/kitten.jpeg',
+                              'asset/jpeg/flower.jpeg',
+                              'asset/jpeg/discoball.jpeg',
+                            ];
+                            return GestureDetector(
+                              onTap: () {
+                                _setFocused(
+                                    index); // Переключаем состояние фокуса для выбранной картинки
+                              },
+                              child: FocusableActionDetector(
+                                onShowFocusHighlight: (focused) =>
+                                    _setFocused(index),
+                                onShowHoverHighlight: (focused) =>
+                                    _setFocused(index),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: _focusedList[index]
+                                        ? BorderRadius.circular(
+                                            32) // Округление для выбранной картинки
+                                        : BorderRadius.circular(
+                                            0), // Без округления для остальных
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(images[index]),
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              );
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ));
             }
           },
         ),
